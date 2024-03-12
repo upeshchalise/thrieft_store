647c1dabf9487887086199045d7a53fb5048e530
upeshchalise <upeshchalise@gmail.com>
2024-03-12 22:49:21 +0545

    start

---
+++ frontend/src/components/Contact.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Contact: React.FC = () => {
+  return (
+    <div className="bg-[#EBEBEB] text-xl flex justify-between px-10">
+      <span className="font-normal">Contact</span>
+      <button className="font-normal">Shop now</button>
+    </div>
+  );
+};
