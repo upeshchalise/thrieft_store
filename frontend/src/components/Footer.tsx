57304045d468d65314944962fe6f623ee62a4fac
upeshchalise <upeshchalise@gmail.com>
2024-05-18 12:23:13 +0545

    git check

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <div className="bg-gray-200 w-full text-center">&copy; thrieft store</div>
+    <div className="bg-gray-200 w-full text-center">
+      &copy; thrieft store test
+    </div>
   );
 };
 
